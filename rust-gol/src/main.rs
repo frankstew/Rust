d96fc3613a80bb4626fd4fa5b95780faa2b5abbe
Franklin Stewart <Franklin.Stewart@tolko.com>
2022-09-07 13:20:21 -0700

    finished game of life, kinda. Some bugs in the rules it seems

--- rust-gol/src/main.rs
+++ rust-gol/src/main.rs
@@ -1,7 +1,7 @@
 use macroquad::prelude::*;
-use macroquad::rand;
+use std::{thread, time};
 
-const GRID_SIZE: usize = 200;
+const GRID_SIZE: usize = 100;
 
 
 fn draw_cell(row: usize, col: usize, is_alive: bool) {
@@ -18,14 +18,139 @@ fn draw_cell(row: usize, col: usize, is_alive: bool) {
     }
 
     draw_rectangle(x, y, cell_width, cell_height, WHITE);
+}
 
+fn get_cell_from_mouse_pos(xpos: f32, ypos: f32) -> (usize, usize) {
+    //i * cellwidth + something = xpos
+    //i = (xpos - something) / cellwodth = xpos / cellwidth - something / cellwidth
+    let cell_width = screen_width() / GRID_SIZE as f32;
+    let cell_height = screen_height() / GRID_SIZE as f32;
+
+    let row = (ypos / cell_height) as usize;
+    let col = (xpos / cell_width) as usize;
+    return (row, col);
+}
+
+fn toggle_cell_alive(mouse_pos: Vec2, grid: &mut Vec<Vec<bool>>) {
+    let mouse_x = mouse_pos[0];
+    let mouse_y = mouse_pos[1];
+
+    let (row, col) = get_cell_from_mouse_pos(mouse_x, mouse_y);
+    grid[row][col] = !grid[row][col];
 
 }
 
+struct Neighbours {
+    alive: i8,
+}
+
+impl Neighbours {
+    fn new() -> Self {
+        let n = Neighbours {
+            alive: 0,
+        };
+        return n;
+    }
+}
+
+fn get_neighbours(row: usize, col: usize, grid: &mut Vec<Vec<bool>>) -> Neighbours {
+    let mut n = Neighbours::new();
+
+    let above = (row + GRID_SIZE - 1) % GRID_SIZE;
+    let below = (row + 1) % GRID_SIZE;
+    let left = (col + GRID_SIZE - 1) % GRID_SIZE;
+    let right = (col + 1) % GRID_SIZE;
+
+
+    let top_row = grid.get(above);
+    match top_row {
+        Some(row) => {
+            match row.get(left) {
+                Some(val) if *val => n.alive+=1,
+                None => {},
+                Some(_) => {}
+            };
+            match row.get(col) {
+                Some(val) if *val => n.alive+=1,
+                None => {},
+                Some(_) => {}
+            };
+            match row.get(right) {
+                Some(val) if *val => n.alive+=1,
+                None => {},
+                Some(_) => {}
+            };
+        },
+        None => {},
+        Some(_) => {}
+    }
+
+    let middle_row = grid.get(row);
+    match middle_row {
+        Some(row) => {
+            match row.get(left) {
+                Some(val) if *val => n.alive+=1,
+                None => {},
+                Some(_) => {}
+            };
+            match row.get(right) {
+                Some(val) if *val => n.alive+=1,
+                None => {},
+                Some(_) => {}
+            };
+        },
+        None => {},
+        Some(_) => {}
+    }
+
+    let bottom_row = grid.get(below);
+    match bottom_row {
+        Some(row) => {
+            match row.get(left) {
+                Some(val) if *val => n.alive+=1,
+                None => {},
+                Some(_) => {}
+            };
+            match row.get(col) {
+                Some(val) if *val => n.alive+=1,
+                None => {},
+                Some(_) => {}
+            };
+            match row.get(right) {
+                Some(val) if *val => n.alive+=1,
+                None => {},
+                Some(_) => {}
+            };
+        },
+        None => {},
+        Some(_) => {}
+    }
+
+    return n;
+}
+
+fn apply_rules(grid: &mut Vec<Vec<bool>>) {
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            let neighbours = get_neighbours(i, j, grid);
+            if (grid[i][j] && (neighbours.alive == 2 || neighbours.alive == 3)) {
+                continue;
+            }
+            if (!grid[i][j] && neighbours.alive == 3) {
+                grid[i][j] = true;
+                continue;
+            }
+            grid[i][j] = false;
+        }
+    }
+
+}
+
+
 #[macroquad::main("rust-gui")]
 async fn main() {
-    
-    let mut grid = vec![vec![0; GRID_SIZE]; GRID_SIZE];
+        let mut grid = vec![vec![false; GRID_SIZE]; GRID_SIZE];
+        let mut run: bool = false;
 
     loop {
         clear_background(WHITE);
@@ -41,17 +166,26 @@ async fn main() {
             draw_line(x, 0.0, x, screen_height(), 1.0, BLACK);
         }
 
-        let cell_width = screen_width() / GRID_SIZE as f32;
-        let cell_height = screen_height() / GRID_SIZE as f32;
+        if (run) {
+            apply_rules(&mut grid);
 
-        draw_cell(0, 0, true);
-        draw_cell(1, 2, true);
-        draw_cell(5, 9, true);
-        draw_cell(7, 2, true);
-        draw_cell(3, 3, true);
-        draw_cell(9, 9, true);
-        draw_cell(5, 5, true);
 
+            thread::sleep(time::Duration::from_secs(1));
+        } 
+
+        if is_mouse_button_pressed(MouseButton::Left) { toggle_cell_alive(mouse_position().into(), &mut grid); }
+
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                draw_cell(i, j, grid[i][j]);
+            }
+        }
+
+        if is_key_pressed(KeyCode::Enter) {
+            run = !run;
+        }
+
+        
         next_frame().await
     }
 }
